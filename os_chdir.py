ff5b43876e172624203fb9d4d698202d0a6d8cb6
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2022-06-08 03:56:41 +0600

    Completed

---
+++ os_chdir.py
@@ -0,0 +1,10 @@
+import os
+
+# change directory to Music
+os.chdir("/home/hridoy/Music")
+
+# print the current working directory
+print(os.getcwd())
+
+# list the files and folders of the directory
+print(os.listdir())
---
+++ os_listdir.py
@@ -0,0 +1,4 @@
+import os
+
+# list of the current working directory
+print(os.listdir())
